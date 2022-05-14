a84dfeb661ac5fc0df61e34f2465eab496d5946d
ninnistic <bewithu14@gmail.com>
2022-05-14 19:48:11 +0900

    클래스의 존재 이유 - 변경 코드, 이유

--- today_I_learnt/src/may_2022/Ep3_TheRaasonOfClass.java
+++ today_I_learnt/src/may_2022/Ep3_TheRaasonOfClass.java
@@ -1,36 +1,43 @@
 package may_2022;
 
+class Print{ 
+	public static String delimiter = "";  
+	
+	public static void A() {
+		System.out.println(delimiter);
+		System.out.println("A");
+		System.out.println("A");
+	}
+	public static void B() {
+		System.out.println(delimiter);
+		System.out.println("B");
+		System.out.println("B");
+	}
+	
+}
+
 public class Ep3_TheRaasonOfClass {
-	
-	
+
+	 
 	// 클래스는 왜 존재해야 할까? 클래스는 왜 필요할까? 그 이유를 잘 생각해보자.
 	
 	public static void main(String[] args) {
-		delimiter = "-------";
-		printA();
-		printA();
-		printB();		
-		printB();		
+		Print.delimiter = "-------";
+		Print.A();
+		Print.A();
+		Print.B();
+		Print.B();
+			
 		
-		delimiter = "******";
-		printA();
-		printA();
-		printB();		
-		printB();	
+		Print.delimiter = "******";
+		Print.A();
+		Print.A();
+		Print.B();
+		Print.B();
 		
 		
 		
 	}
-	public static String delimiter = "";
-	public static void printA() {
-		System.out.println(delimiter);
-		System.out.println("A");
-		System.out.println("A");
-	}
-	public static void printB() {
-		System.out.println(delimiter);
-		System.out.println("B");
-		System.out.println("B");
-	}
+	
 
 }
