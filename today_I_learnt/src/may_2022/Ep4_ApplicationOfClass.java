ef21de844616babdf7f70548b1f5af45e94da079
ninnistic <bewithu14@gmail.com>
2022-05-14 20:10:10 +0900

    클래스의 활용 - Understanding

--- today_I_learnt/src/may_2022/Ep4_ApplicationOfClass.java
+++ today_I_learnt/src/may_2022/Ep4_ApplicationOfClass.java
@@ -1,10 +1,35 @@
 package may_2022;
 
+class Accounting  {
+    public static double valueOfSupply;
+    
+    public static double vatRate = 0.1;
+ 
+    public static double getVAT() {
+        return valueOfSupply * vatRate;
+    }
+     
+    public static double getTotal() {
+        return valueOfSupply + getVAT();
+    }
+}
+
+
 public class Ep4_ApplicationOfClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+// 장사를 하는 중인데, 물건가격이 만원이라면, 원가의 10퍼센트의 돈을 더 받아야한다. (부가가치세) 
+	
+	// Actual class name : AccountingApp
+	
+	   
+	 
+	    public static void main(String[] args) {
+	    	
+	    	Accounting.valueOfSupply = 10000.0;
+	        System.out.println("Value of supply : " + Accounting.valueOfSupply);
+	        System.out.println("VAT : " + Accounting.getVAT());
+	        System.out.println("Total : " + Accounting.getTotal());
+	 
+	    }
 	}
 
 }
